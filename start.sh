78c8b6d0248737f9a184a1cfc8515db2ba019623
shobonvip <shobonvip@gmail.com>
2026-07-16 09:37:51 +0900

    げみにPORTは80だよ

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # 1. コンテナ外部からのアクセスを許可する (Next.js特有の502エラー対策)
 export HOSTNAME="0.0.0.0"
-export PORT=3000
+export PORT=80
 
 # 2. Next.js / Prisma Client 実行用に DATABASE_URL を OS レベルで合成
 export DATABASE_URL="mysql://${NS_MARIADB_USER}:${NS_MARIADB_PASSWORD}@${NS_MARIADB_HOSTNAME}:${NS_MARIADB_PORT}/${NS_MARIADB_DATABASE}"
